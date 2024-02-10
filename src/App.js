794794af1ff89eabe42ff9bc2d41504ed11b28b8
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-02-11 00:21:35 +0100

    main components done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,77 @@
-function App() {
-  return <div className="App"></div>;
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+  { id: 4, description: "Chargerusb", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🧳 get packed 👜</h1>;
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip</h3>
+      <select>
+        {/* tworzenie tablicy z liczbami od 1 do 20 a potem jej zmapowanie*/}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..."></input>
+      {/* mozna submitować formularz dodając do przycisku atrybut onClick={handleSubmit} ale działa tylko na kliknięcie bez obsługi klawisza "enter" */}
+      <button>ADD</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox"></input>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      You have X item on your list, and you already packed X (X%)
+    </footer>
+  );
+}
